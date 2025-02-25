78950b4f6db87415579e4b9abbdfcc171fe21aa3
Tamara Beruashvili <tomaberuashvili@gmail.com>
2025-02-25 23:19:17 +0300

    Now Clock class Components  manages its own time state. This moves time management from App to Clock, restoring previous behavior

--- src/App.tsx
+++ src/App.tsx
@@ -4,88 +4,50 @@ import { Clock } from './components/Clock';
 
 function getRandomName(): string {
   const value = Date.now().toString().slice(-4);
+
   return `Clock-${value}`;
 }
 
-type State = {
-  clockName: string;
-  currentTime: string;
-  hasClock: boolean;
-};
-
-export class App extends React.Component<{}, State> {
-  intervalId: NodeJS.Timeout | null = null;
-  timerId: NodeJS.Timeout | null = null;
-
-  state: State = {
-    clockName: 'Clock-0',
-    currentTime: new Date().toUTCString().slice(-12, -4),
+export class App extends React.Component {
+  state = {
     hasClock: true,
+    clockName: 'Clock-0',
   };
 
-  componentDidMount() {
-    this.startTimer();
-    this.startClock();
-    document.addEventListener('contextmenu', this.handleRightClick);
-    document.addEventListener('click', this.handleLeftClick);
-  }
+  nameTimerId: NodeJS.Timeout | null = null;
 
-  componentWillUnmount() {
-    this.stopTimer();
-    this.stopClock();
-    document.removeEventListener('contextmenu', this.handleRightClick);
-    document.removeEventListener('click', this.handleLeftClick);
-  }
-
-  startTimer() {
-    this.intervalId = setInterval(() => {
-      const oldName = this.state.clockName;
-      const newName = getRandomName();
-      this.setState({ clockName: newName });
-      // eslint-disable-next-line no-console
-      console.warn(`Renamed from ${oldName} to ${newName}`);
-    }, 3300);
-  }
-
-  stopTimer() {
-    if (this.intervalId) {
-      clearInterval(this.intervalId);
-      this.intervalId = null;
-    }
-  }
-
-  startClock() {
-    this.timerId = setInterval(() => {
-      const currentTime = new Date().toUTCString().slice(-12, -4);
-      this.setState({ currentTime });
-      // eslint-disable-next-line no-console
-      console.log(currentTime);
-    }, 1000);
-  }
-
-  stopClock() {
-    if (this.timerId) {
-      clearInterval(this.timerId);
-      this.timerId = null;
-    }
-  }
-
-  handleRightClick = (event: MouseEvent) => {
+  handleContextMenu = (event: MouseEvent) => {
     event.preventDefault();
     this.setState({ hasClock: false });
   };
 
-  handleLeftClick = () => {
+  handleClick = () => {
     this.setState({ hasClock: true });
   };
 
-  render() {
-    const { clockName, currentTime, hasClock } = this.state;
+  componentDidMount() {
+    document.addEventListener('contextmenu', this.handleContextMenu);
+    document.addEventListener('click', this.handleClick);
 
+    this.nameTimerId = setInterval(() => {
+      this.setState({ clockName: getRandomName() });
+    }, 3300);
+  }
+
+  componentWillUnmount() {
+    if (this.nameTimerId !== null) {
+      clearInterval(this.nameTimerId);
+    }
+
+    document.removeEventListener('contextmenu', this.handleContextMenu);
+    document.removeEventListener('click', this.handleClick);
+  }
+
+  render() {
     return (
       <div className="App">
         <h1>React clock</h1>
-        {hasClock && <Clock name={clockName} time={currentTime} />}
+        {this.state.hasClock && <Clock name={this.state.clockName} />}
       </div>
     );
   }
